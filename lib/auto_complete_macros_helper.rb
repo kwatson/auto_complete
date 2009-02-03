e535712dc068a47631379a3b880bc01edcd2d588
Kris Watson <kristian.watson@gmail.com>
2009-02-02 18:24:24 -0800

    Now supports model accessors

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -95,7 +95,10 @@ module AutoCompleteMacrosHelper
   # auto_complete action if you need to decorate it further.
   def auto_complete_result(entries, field, phrase = nil)
     return unless entries
-    items = entries.map { |entry| content_tag("li", phrase ? highlight(entry[field], phrase) : h(entry[field])) }
+    items = entries.map { |entry| 
+      text = entry.is_a?(Hash) ? entry[field] : entry.send(field)
+      content_tag("li", phrase ? highlight(text, phrase) : h(text))
+    }
     content_tag("ul", items.uniq)
   end
   
